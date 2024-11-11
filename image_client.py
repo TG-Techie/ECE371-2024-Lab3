bfd4d7ece07db9db1faed312f8d1283c990eaf08
keshavVerma11 <75388669+keshavVerma11@users.noreply.github.com>
2024-11-11 18:46:41 -0500

    Initial Part 5 Changes

--- image_client.py
+++ image_client.py
@@ -1,7 +1,10 @@
-import des
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM, gethostbyname
 from RSA import generate_keypair, encrypt, decrypt
+
+import RSA as rsa
+import des
+
 import struct
 
 
@@ -24,18 +27,23 @@ p = 1297273
 q = 1297651
 ###################################your code goes here#####################################
 # generate public and private key from the p and q values
-public = [0, 0]
-private = [0, 0]
-# send key
+# returns  ((e, n), (d, n))
+public_key, private_key = rsa.generate_keypair(p, q)
 
-message = "public_key: %d %d" % (public[0], public[1])
+# send key
+pub_e, pub_n = public_key
+message = """(public_key{"e": %d, "n": %d}public_key)""" % (pub_e, pub_n)
+print("sending", repr(message))
 mySocket.sendto(message.encode(), (SERVER_IP, PORT_NUMBER))
+
 # send des_key
 message = "des_key"
 mySocket.sendto(message.encode(), (SERVER_IP, PORT_NUMBER))
 ###################################your code goes here#####################################
 # encode the DES key with RSA and save in DES_encoded, the value below is just an example
-des_encoded = ["2313", "3231", "532515", "542515", "5135151", "31413", "15315", "14314"]
+des_encoded = [str(rsa.encrypt(private_key, char)) for char in message]
+#des_encoded = ["2313", "3231", "532515", "542515", "5135151", "31413", "15315", "14314"]
+
 [mySocket.sendto(code.encode(), (SERVER_IP, PORT_NUMBER)) for code in des_encoded]
 # read image, encode, send the encoded image binary file
 file = open(r"penguin.jpg", "rb")
@@ -47,6 +55,7 @@ file.close()
 # coder=des.des(), use bytearray to send the encryped image through network
 # r_byte is the final value you will send through socket
 r_byte = bytearray()
+
 
 # send image through socket
 mySocket.sendto(bytes(r_byte), (SERVER_IP, PORT_NUMBER))
